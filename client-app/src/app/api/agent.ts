bb500cfc262ef62f07f1a95510f1d71285bdb0d7
David John <davidajohn@gmail.com>
2021-10-06 23:14:39 +0100

    Created auth methods and interfaces

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,8 +1,10 @@
+import { UserFormValues } from './../models/user';
 import { history } from './../../index';
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { Activity } from "../models/activity";
 import { store } from '../stores/store';
+import { User } from '../models/user';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -74,8 +76,15 @@ const Activities = {
     delete: (id: string) => requests.del<void>(`/activities/${id}`)
 }
 
+const Account = {
+    current: () => requests.get<User>('/account'),
+    login: (user: UserFormValues) => requests.post<User>('/account/login', user),
+    register: (user: UserFormValues) => requests.post<User>('/account/register', user),
+}
+
 const agent = {
-    Activities
+    Activities,
+    Account
 }
 
 export default agent;
