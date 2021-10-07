dfae638c648b8736a17d60a6471a38507b592e74
David John <davidajohn@gmail.com>
2021-10-07 15:37:57 +0100

    Added a user store to mobx for authentication

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,21 +1,25 @@
 import { Form, Formik } from 'formik'
+import { observer } from 'mobx-react-lite'
 import React from 'react'
 import { Button } from 'semantic-ui-react'
 import { TextInput } from '../../app/common/form/TextInput'
+import { useStore } from '../../app/stores/store'
 
-export const LoginForm = () => {
+export default observer(function LoginForm()  {
+    const {userStore} = useStore();
+
     return (
         <Formik
             initialValues={{email: '', password: ''}}
-            onSubmit={values => console.log(values)}
+            onSubmit={values => userStore.login(values)}
         >
-            {({handleSubmit}) => (
+            {({handleSubmit, isSubmitting}) => (
                 <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
                     <TextInput name='email' placeholder='Email' />
                     <TextInput name='password' placeholder='Password' type='password' />
-                    <Button positive content='Login' type='submit' fluid />
+                    <Button loading={isSubmitting} positive content='Login' type='submit' fluid />
                 </Form>
             )}
         </Formik>
     )
-}
+})
