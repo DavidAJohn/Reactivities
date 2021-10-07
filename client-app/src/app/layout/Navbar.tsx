5fcfd33c486f74d46dd43200729b5d113f7f72d6
David John <davidajohn@gmail.com>
2021-10-07 19:21:01 +0100

    Added navbar dropdown with user options

--- client-app/src/app/layout/Navbar.tsx
+++ client-app/src/app/layout/Navbar.tsx
@@ -1,8 +1,12 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react'
-import { NavLink } from 'react-router-dom';
-import { Button, Container, Menu, MenuItem } from 'semantic-ui-react'
+import { Link, NavLink } from 'react-router-dom';
+import { Button, Container, Dropdown, DropdownItem, DropdownMenu, Image, Menu, MenuItem } from 'semantic-ui-react'
+import { useStore } from '../stores/store';
 
-export const Navbar = () => {
+export const Navbar = observer(() => {
+    const {userStore: {user, logout}} = useStore();
+
     return (
         <Menu inverted fixed='top'>
             <Container>
@@ -19,7 +23,16 @@ export const Navbar = () => {
                 <MenuItem header>
                     <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
                 </MenuItem>
+                <MenuItem position='right'>
+                    <Image src={user?.image || 'assets/user.png'} avatar spaced='right' />
+                    <Dropdown pointing='top left' text={user?.displayName}>
+                        <DropdownMenu>
+                            <DropdownItem as={Link} to={`/profile/${user?.username}`} text='My Profile' icon='user'></DropdownItem>
+                            <DropdownItem onClick={logout} text='Log Out' icon='power'></DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                </MenuItem>
             </Container>
         </Menu>
     )
-}
+});
