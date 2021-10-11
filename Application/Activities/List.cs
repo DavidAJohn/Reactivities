8a387852c68170778371a70dc9d39eeb8ab55c8b
David John <davidajohn@gmail.com>
2021-10-11 18:16:51 +0100

    Updated API to pull in related activity & user data

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -11,22 +12,29 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<Activity>>> {}
+        public class Query : IRequest<Result<List<ActivityDTO>>> {}
 
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDTO>>>
         {
             private readonly DataContext _context;
+            private readonly IMapper _mapper;
             
-            public Handler(DataContext context)
+            public Handler(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
             }
 
-            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<ActivityDTO>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activities = await _context.Activities.ToListAsync(cancellationToken);
+                var activities = await _context.Activities
+                    .Include(a => a.Attendees)
+                    .ThenInclude(u => u.AppUser)
+                    .ToListAsync(cancellationToken);
 
-                return Result<List<Activity>>.Success(activities);
+                var activitiesToReturn = _mapper.Map<List<ActivityDTO>>(activities);
+
+                return Result<List<ActivityDTO>>.Success(activitiesToReturn);
             }
         }
     }
