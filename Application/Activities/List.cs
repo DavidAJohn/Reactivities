d5c8e210cb5c166aaefb45ac009296d33ea27a9a
David John <davidajohn@gmail.com>
2021-10-11 19:01:49 +0100

    Updated AutoMapper profiles to shape returned data

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -28,13 +29,10 @@ namespace Application.Activities
             public async Task<Result<List<ActivityDTO>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activities = await _context.Activities
-                    .Include(a => a.Attendees)
-                    .ThenInclude(u => u.AppUser)
+                    .ProjectTo<ActivityDTO>(_mapper.ConfigurationProvider)
                     .ToListAsync(cancellationToken);
 
-                var activitiesToReturn = _mapper.Map<List<ActivityDTO>>(activities);
-
-                return Result<List<ActivityDTO>>.Success(activitiesToReturn);
+                return Result<List<ActivityDTO>>.Success(activities);
             }
         }
     }
