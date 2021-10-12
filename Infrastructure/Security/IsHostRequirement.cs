38d13688b36a973b6491cbbc04e260dd02f1be27
David John <davidajohn@gmail.com>
2021-10-12 15:22:58 +0100

    Fixed issue with editing and attendees

--- Infrastructure/Security/IsHostRequirement.cs
+++ Infrastructure/Security/IsHostRequirement.cs
@@ -5,6 +5,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Infrastructure.Security
@@ -33,7 +34,10 @@ namespace Infrastructure.Security
                 x => x.Key == "id").Value?.ToString()
             );
 
-            var attendee = _dbContext.ActivityAttendees.FindAsync(userId, activityId).Result;
+            var attendee = _dbContext.ActivityAttendees
+                .AsNoTracking()
+                .SingleOrDefaultAsync(x => x.AppUserId == userId && x.ActivityId == activityId)
+                .Result;
 
             if (attendee == null) return Task.CompletedTask;
 
