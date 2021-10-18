8249f129f6761dd9ce189463d39254da3cab2432
David John <davidajohn@gmail.com>
2021-10-18 21:27:30 +0100

    Added Cloudinary package and configured

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,6 +6,7 @@ using Application.Core;
 using Application.Interfaces;
 using Domain;
 using FluentValidation.AspNetCore;
+using Infrastructure.Photos;
 using Infrastructure.Security;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -93,6 +94,7 @@ namespace API
 
             services.AddScoped<TokenService>();
             services.AddScoped<IUserAccessor, UserAccessor>();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
