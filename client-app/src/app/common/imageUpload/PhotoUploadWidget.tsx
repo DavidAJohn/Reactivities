6905eaf8e1025400bbe69bda34fd39de6c3a84d8
David John <davidajohn@gmail.com>
2021-10-27 00:24:40 +0100

    Added styling to the photo dropzone

--- client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import { Grid, GridColumn, Header } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Grid, GridColumn, Header, Image } from 'semantic-ui-react'
 import { PhotoWidgetDropzone } from './PhotoWidgetDropzone'
 
 export const PhotoUploadWidget = () => {
+    const [files, setFiles] = useState<any>([]);
+    
     return (
         <Grid>
             <GridColumn width={4}>
                 <Header color='teal' content='Step 1 - Add Photo' />
-                <PhotoWidgetDropzone />
+                <PhotoWidgetDropzone setFiles={setFiles} />
             </GridColumn>
             <GridColumn width={1} />
             <GridColumn width={4}>
                 <Header color='teal' content='Step 2 - Resize Image' />
+                {files && files.length > 0 && (
+                    <Image src={files[0].preview} />
+                )}
             </GridColumn>
             <GridColumn width={1} />
             <GridColumn width={4}>
