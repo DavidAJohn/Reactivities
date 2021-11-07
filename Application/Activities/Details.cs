e4c38239ec2401811520ffa15b8e63804187db04
David John <davidajohn@gmail.com>
2021-11-07 12:52:45 +0000

    Updated other follow-related handlers

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Domain;
@@ -22,9 +23,11 @@ namespace Application.Activities
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context, IMapper mapper)
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _mapper = mapper;
                 _context = context;
             }
@@ -32,7 +35,7 @@ namespace Application.Activities
             public async Task<Result<ActivityDTO>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activity =  await _context.Activities
-                    .ProjectTo<ActivityDTO>(_mapper.ConfigurationProvider)
+                    .ProjectTo<ActivityDTO>(_mapper.ConfigurationProvider, new {currentUsername = _userAccessor.GetUsername()})
                     .FirstOrDefaultAsync(x => x.Id == request.Id);
                 
                 return Result<ActivityDTO>.Success(activity);
