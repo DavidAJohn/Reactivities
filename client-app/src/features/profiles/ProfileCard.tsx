439af3799fc044f8e600c9c7fce40e8d4f42a659
David John <davidajohn@gmail.com>
2021-11-08 18:06:50 +0000

    Converted follow button into a component

--- client-app/src/features/profiles/ProfileCard.tsx
+++ client-app/src/features/profiles/ProfileCard.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Card, CardContent, CardDescription, CardHeader, Icon, Image } from 'semantic-ui-react'
 import { Profile } from '../../app/models/profile'
+import { FollowButton } from './FollowButton'
 
 interface Props {
     profile: Profile;
@@ -27,6 +28,7 @@ export const ProfileCard = observer(({profile}: Props) => {
                 <Icon name='user' />
                 {profile.followersCount} {profile.followersCount === 1 ? 'follower' : 'followers'}
             </CardContent>
+            <FollowButton profile={profile} />
         </Card>
     )
 })
--- client-app/src/features/profiles/ProfileHeader.tsx
+++ client-app/src/features/profiles/ProfileHeader.tsx
@@ -1,8 +1,9 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react'
-import { Button, Divider, Grid, GridColumn, Header, Item, ItemContent, 
-    ItemGroup, ItemImage, Reveal, RevealContent, Segment, Statistic, StatisticGroup } from 'semantic-ui-react'
+import { Divider, Grid, GridColumn, Header, Item, ItemContent, 
+    ItemGroup, ItemImage, Segment, Statistic, StatisticGroup } from 'semantic-ui-react'
 import { Profile } from '../../app/models/profile'
+import { FollowButton } from './FollowButton';
 
 interface Props {
     profile: Profile;
@@ -28,19 +29,7 @@ export const ProfileHeader = observer(({profile}: Props) => {
                         <Statistic label='Following' value={profile.followingCount} />
                     </StatisticGroup>
                     <Divider />
-                    <Reveal animated='move'>
-                        <RevealContent visible style={{width: '100%'}}>
-                            <Button fluid color='teal' content='Following'></Button>
-                        </RevealContent>
-                        <RevealContent hidden style={{width: '100%'}}>
-                            <Button 
-                                fluid 
-                                basic
-                                color={true ? 'red' : 'green'} 
-                                content={true ? 'Unfollow' : 'Follow'}
-                            />
-                        </RevealContent>
-                    </Reveal>
+                    <FollowButton profile={profile} />
                 </GridColumn>
             </Grid>
         </Segment>
