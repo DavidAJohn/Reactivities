29c6f6ab43a45b9f655f8ec741ae5b758107b946
David John <davidajohn@gmail.com>
2021-11-11 11:46:45 +0000

    Added pagination basics to the client app

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,3 +1,4 @@
+import { PaginatedResult } from './../models/pagination';
 import { Photo } from './../models/profile';
 import { UserFormValues } from './../models/user';
 import { history } from './../../index';
@@ -26,7 +27,16 @@ axios.interceptors.request.use(config => {
 
 axios.interceptors.response.use(async response => {
     await sleep(1000);
+
+    const pagination = response.headers['pagination'];
+
+    if (pagination) {
+        response.data = new PaginatedResult(response.data, JSON.parse(pagination));
+        return response as AxiosResponse<PaginatedResult<any>>;
+    }
+
     return response;
+
 }, (error: AxiosError) => {
     const {data, status, config} = error.response!;
 
@@ -79,7 +89,7 @@ const requests = {
 }
 
 const Activities = {
-    list: () => requests.get<Activity[]>('/activities'),
+    list: () => requests.get<PaginatedResult<Activity[]>>('/activities'),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
     create: (activity: ActivityFormValues) => requests.post<void>('/activities/', activity),
     update: (activity: ActivityFormValues) => requests.put<void>(`/activities/${activity.id}`, activity),
