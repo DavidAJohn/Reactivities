ab90a67a2e70bcad10e47478eb1f7e4bc3a7b476
David John <davidajohn@gmail.com>
2021-11-17 12:23:11 +0000

    Configured API to serve client app via fallback controller

--- API/Startup.cs
+++ API/Startup.cs
@@ -135,6 +135,9 @@ namespace API
 
             app.UseRouting();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseCors("CorsPolicy");
 
             app.UseAuthentication();
@@ -144,6 +147,7 @@ namespace API
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<ChatHub>("/chat");
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
