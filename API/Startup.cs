d5579fc233068188ecb6d7163c1b542ecda958ce
David John <davidajohn@gmail.com>
2021-11-18 23:51:03 +0000

    Removed the fallback controller and static file config

--- API/Startup.cs
+++ API/Startup.cs
@@ -135,9 +135,6 @@ namespace API
 
             app.UseRouting();
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-
             app.UseCors("CorsPolicy");
 
             app.UseAuthentication();
@@ -147,7 +144,6 @@ namespace API
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<ChatHub>("/chat");
-                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
